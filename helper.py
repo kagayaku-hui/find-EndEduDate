4dc7938af52dc683225598cb6c5f8dac730632ce
kagayaku-hui <41921165+kagayaku-hui@users.noreply.github.com>
2018-07-30 13:00:29 -0400

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,13 @@
+class partner:
+    dates = []
+    partner_id = 0
+    def __init__(self, partner_id):
+        self.partner_id = partner_id
+class date:
+    startDate = 0
+    endDate = 0
+    def __init__(self, startDate, endDate):
+        self.startDate = startDate
+        self.endDate = endDate
+
+
